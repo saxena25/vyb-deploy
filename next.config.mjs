ae4287ee5986999683edb8cbb2f9048fc6ea72e1
unknown <saxenahrithik2000@gmail.com>
2024-08-24 12:04:35 +0530

    added a FAQ section

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withVideos from "next-videos";
+const nextConfig = {
+    images: {
+        domains: ['d1muf25xaso8hp.cloudfront.net'],
+      }
+};
 
-export default nextConfig;
+// export default {...nextConfig,...withVideos};
+export default withVideos(nextConfig);
+
+
+
+
